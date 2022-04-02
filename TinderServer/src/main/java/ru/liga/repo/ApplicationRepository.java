1a28321f61d95189d8eda776244fddfee5c6c2b3
Andrey Savelov <udroguedozed@gmail.com>
2022-04-02 14:59:57 +0300

    ImageService + LongPollingBot

--- TinderServer/src/main/java/ru/liga/repo/ApplicationRepository.java
+++ TinderServer/src/main/java/ru/liga/repo/ApplicationRepository.java
@@ -2,6 +2,7 @@ package ru.liga.repo;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.method.P;
 import ru.liga.domain.Profile;
 import ru.liga.domain.SexType;
@@ -11,10 +12,10 @@ import java.util.Set;
 
 public interface ApplicationRepository extends CrudRepository<Profile, Long> {
 
-    @Query(value = "select * from profile\n" +
-            "where sex_type in (select looking_for from sex_type where profile_id = :profileId)\n" +
-            "and id <> :profileId\n" +
-            "and id not in (select to_profile from profile_likes where from_profile = :profileId)\n" +
-            "and id in (select profile_id from sex_type where looking_for = :sexType)", nativeQuery = true)
-    Set<Profile> findAllValidProfilesForUser(Long profileId, int sexType);
+    @Query(value = "select * from tinder_db.profile " +
+            "where sex_type in (select looking_for from tinder_db.sex_type where profile_id = :profileId)" +
+            "and (id <> :profileId)" +
+            "and id not in (select to_profile from tinder_db.profile_likes where from_profile = :profileId)" +
+            "and id in (select profile_id from tinder_db.sex_type where looking_for = :sexType)", nativeQuery = true)
+    Set<Profile> findAllValidProfilesForUser(@Param("profileId") Long profileId, @Param("sexType") int sexType);
 }
